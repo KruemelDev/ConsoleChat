65df3b9056d597937f7a8cd49a78d74c7ef01068
THMKruemel <lennartsigge@icloud.com>
2023-07-20 15:41:57 +0200

    add hashing of password. Make everything oop. Implement multithreading

--- Client/client.py
+++ Client/client.py
@@ -1,29 +1,51 @@
 import socket
+import threading
+import bcrypt
 
 
-def main():
+class Client:
+    def __init__(self):
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_address = ('localhost', 5050)
+        self.client_socket.connect(self.server_address)
 
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_address = ('localhost', 5050)
-    client_socket.connect(server_address)
+    @staticmethod
+    def hash_password(password):
+        salt = bcrypt.gensalt()
+        hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+        return hashed_password
 
-    while True:
-        check_account = input("Do you already have an account? If you have one type: signin else type: register.")
-        if check_account == "signin":
-            print()
-            username = input("Type your username")
-            password = input("Type your password")
-            account_data = (username, password)
-            client_socket.send(bytes("Sign in with{}".format(account_data), "utf8"))
-        elif check_account == "register":
-            print()
-            username = input("Type a name how your friends will see you")
-            password = input("Type a password")
-            register_data = (username, password)
-            client_socket.send(bytes("Register with {}".format(register_data), "utf8"))
+    def login(self):
+        while True:
+            check_account = input("Do you already have an account? If you have one type: signin else type: register.")
+            if check_account == "signin":
+                print()
+                self.client_socket.send(bytes("signin", "utf8"))
 
-        else:
-            continue
+                username = input("Type your username")
+                password = input("Type your password")
+
+                hashed_password = self.hash_password(password)
+                account_data = (username, hashed_password)
+                self.client_socket.send(bytes("Sign in with{}".format(account_data), "utf8"))
+                break
+            elif check_account == "register":
+                print()
+                self.client_socket.send(bytes("register", "utf8"))
+
+                username = input("Type a name how your friends will see you")
+                password = input("Type a password")
+
+                hashed_password = self.hash_password(password)
+                register_data = (username, hashed_password)
+                self.client_socket.send(bytes("Register with {}".format(register_data), "utf8"))
+                break
+            else:
+                continue
+
 
 if __name__ == "__main__":
-    main()
+    client = Client()
+    login_thread = threading.Thread(target=client.login())
+    login_thread.start()
+    login_thread.join()
--- Server/server.py
+++ Server/server.py
@@ -1,5 +1,6 @@
 import socket
 import mysql.connector
+import threading
 
 mydb = mysql.connector.connect(
     host="localhost",
@@ -9,17 +10,48 @@ mydb = mysql.connector.connect(
 )
 
 
-def main():
-    mycursor = mydb.cursor(buffered=True)
-    mycursor.execute("CREATE DATABASE IF NOT EXISTS consolechat")
-    mycursor.execute("USE consolechat")
-    mycursor.execute("CREATE TABLE IF NOT EXISTS {} (id INT AUTO_INCREMENT PRIMARY KEY, username VARCHAR(255), password VARCHAR(255))".format("Users"))
+class Server:
+    def __init__(self):
+        mycursor = mydb.cursor(buffered=True)
+        mycursor.execute("CREATE DATABASE IF NOT EXISTS consolechat")
+        mycursor.execute("USE consolechat")
+        mycursor.execute("CREATE TABLE IF NOT EXISTS {} (id INT AUTO_INCREMENT PRIMARY KEY, username VARCHAR(255), password VARCHAR(255), ip VARCHAR(255), port INT(255))".format("Users"))
 
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_address = ('localhost', 5050)
-    server_socket.bind(server_address)
-    server_socket.listen(5)
+        self.clients = {}
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_address = ('localhost', 5050)
+        self.server_socket.bind(self.server_address)
+        self.server_socket.listen(5)
 
+    def main(self):
+        print("i am in main")
+        while True:
+            self.client_socket, self.client_address = self.server_socket.accept()
+            print(f"Connection established from {self.client_address}")
+            self.clients[self.client_socket] = self.client_address
+            print(self.clients)
+            handle_thread = threading.Thread(target=self.handle_client)
+            handle_thread.start()
+
+    def handle_client(self):
+        self.clients[self.client_socket] = self.client_address
+        print(self.clients)
+        while True:
+            self.msg = self.client_socket.recv(1024)
+            print(self.msg)
+            if not self.msg:
+                # Client disconnected
+                break
+            store_thread = threading.Thread(target=self.store_in_db())
+            store_thread.start()
+            print(f"Received message from {self.client_address}: {self.msg.decode('utf-8')}")
+
+    def store_in_db(self):
+        if self.msg.decode('utf-8') == "register":
+            if self.msg.decode('utf-8')[::]:
+                pass
 
 if __name__ == "__main__":
-    main()
+    server = Server()
+    main_thread = threading.Thread(target=server.main())
+    main_thread.start()
