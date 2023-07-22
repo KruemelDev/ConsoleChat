fc176c1b34b3002c5b66e3e3b6da70a52d9992da
THMKruemel <lennartsigge@icloud.com>
2023-07-23 00:06:32 +0200

    finish registration system

--- Client/client.py
+++ Client/client.py
@@ -1,12 +1,13 @@
 import socket
 import threading
 import bcrypt
+import json
 
 
 class Client:
     def __init__(self):
         self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server_address = ('localhost', 5050)
+        self.server_address = ('localhost', 12345)
         self.client_socket.connect(self.server_address)
 
     @staticmethod
@@ -20,26 +21,34 @@ class Client:
             check_account = input("Do you already have an account? If you have one type: signin else type: register.")
             if check_account == "signin":
                 print()
-                self.client_socket.send(bytes("signin", "utf8"))
+                self.client_socket.send(bytes("!signin", "utf8"))
 
                 username = input("Type your username")
                 password = input("Type your password")
 
-                hashed_password = self.hash_password(password)
-                account_data = (username, hashed_password)
-                self.client_socket.send(bytes("Sign in with{}".format(account_data), "utf8"))
+                hashed_password = self.hash_password(password).decode("utf8")
+                register_data = {username: "{}".format(username), password: "{}".format(hashed_password)}
+                json_data = json.dumps(register_data)
+                self.client_socket.send(bytes("".format(json_data), "utf8"))
                 break
             elif check_account == "register":
                 print()
-                self.client_socket.send(bytes("register", "utf8"))
+                self.client_socket.send(bytes("!register", "utf8"))
 
-                username = input("Type a name how your friends will see you")
-                password = input("Type a password")
+                username_input = input("Type a name how your friends will see you")
+                password_input = input("Type a password")
 
-                hashed_password = self.hash_password(password)
-                register_data = (username, hashed_password)
-                self.client_socket.send(bytes("Register with {}".format(register_data), "utf8"))
-                break
+                hashed_password = self.hash_password(password_input).decode("utf8")
+                register_data = {"username": username_input, "password": hashed_password}
+                json_data = json.dumps(register_data)
+                self.client_socket.send(json_data.encode("utf8"))
+                server_answer = self.client_socket.recv(1024).decode("utf-8")
+                print(server_answer)
+                if server_answer.startswith("!Already taken"):
+                    continue
+                elif server_answer.startswith("!Successful"):
+                    break
+                    # code if you are logged in
             else:
                 continue
 
--- Server/server.py
+++ Server/server.py
@@ -1,27 +1,31 @@
 import socket
 import mysql.connector
 import threading
-
-mydb = mysql.connector.connect(
-    host="localhost",
-    port="3308",
-    user="root",
-    passwd="spit-preface-inflict-nonsense-hooves-heaven-noted-pitcher-skyway-choice"
-)
+import json
 
 
 class Server:
     def __init__(self):
-        mycursor = mydb.cursor(buffered=True)
-        mycursor.execute("CREATE DATABASE IF NOT EXISTS consolechat")
-        mycursor.execute("USE consolechat")
-        mycursor.execute("CREATE TABLE IF NOT EXISTS {} (id INT AUTO_INCREMENT PRIMARY KEY, username VARCHAR(255), password VARCHAR(255), ip VARCHAR(255), port INT(255))".format("Users"))
+        self.mydb = mysql.connector.connect(
+            host="localhost",
+            port="3308",
+            user="root",
+            passwd="spit-preface-inflict-nonsense-hooves-heaven-noted-pitcher-skyway-choice"
+        )
+
+        self.mycursor = self.mydb.cursor(buffered=True)
+        self.mycursor.execute("CREATE DATABASE IF NOT EXISTS consolechat")
+        self.mycursor.execute("USE consolechat")
+        self.mycursor.execute("CREATE TABLE IF NOT EXISTS {} (id INT AUTO_INCREMENT PRIMARY KEY, username VARCHAR(255), password VARCHAR(255), ip VARCHAR(255), port INT(255))".format("Users"))
 
         self.clients = {}
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server_address = ('localhost', 5050)
+        self.server_address = ('localhost', 12345)
         self.server_socket.bind(self.server_address)
         self.server_socket.listen(5)
+
+        self.register_process = False
+        self.signin_process = False
 
     def main(self):
         print("i am in main")
@@ -42,16 +46,53 @@ class Server:
             if not self.msg:
                 # Client disconnected
                 break
-            store_thread = threading.Thread(target=self.store_in_db())
-            store_thread.start()
+
             print(f"Received message from {self.client_address}: {self.msg.decode('utf-8')}")
 
-    def store_in_db(self):
-        if self.msg.decode('utf-8') == "register":
-            if self.msg.decode('utf-8')[::]:
-                pass
+            received_data = self.msg.decode('utf-8')
+            if received_data.startswith("!signin") or self.signin_process:
+                if received_data.startswith("!signin"):
+                    self.signin_process = True
+                    continue
+                json_data = json.loads(received_data)
+                print(json_data)
+                self.signin_process = False
+
+            if received_data.startswith("!register") or self.register_process:
+                if received_data.startswith("!register"):
+                    self.register_process = True
+                    continue
+                json_data = json.loads(received_data)
+                print(json_data)
+                self.register_process = False
+                if self.is_user_registered(json_data["username"]):
+                    self.client_socket.send(bytes("!Already taken", "utf8"))
+                else:
+
+                    # Args cannot be overgiven
+                    store_thread = threading.Thread(target=self.register_in_db(), args=(json_data))
+                    self.client_socket.send(bytes("!Successful", "utf8"))
+
+                # store_thread = threading.Thread(target=self.store_in_db())
+                # store_thread.start()
+
+    def is_user_logged_in(self):
+        pass
+
+    def is_user_registered(self, username):
+        self.mycursor.execute("SELECT username FROM Users WHERE username = %s", (username,))
+        result = self.mycursor.fetchone()
+        return result is not None
+
+    def register_in_db(self, user_credentials):
+        self.mycursor.execute("INSERT INTO Users (username, password, ip, port) VALUES (%s, %s, %s, %s)",
+                              (user_credentials["username"], user_credentials["password"], self.client_address[0], self.client_address[1]))
+        self.mydb.commit()
+
 
 if __name__ == "__main__":
     server = Server()
     main_thread = threading.Thread(target=server.main())
     main_thread.start()
+
+# This just works for one person at once at the moment
